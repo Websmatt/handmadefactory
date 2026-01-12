cf94c559bea31591229dce7849ebbad9efa035f2
zaqu <zaqu@MacBook-Air-zaqu.local>
2026-01-12 23:20:47 +0100

    Version before removing alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from app.db import Base
-from app import models  # noqa: F401
+import app.models  # noqa: F401
 
 config = context.config
 try:
